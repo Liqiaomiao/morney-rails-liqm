bd67d73086ec2c8ca33fefdf7b670608199b0d63
Liqiaomiao <1049259456@qq.com>
2020-06-30 09:06:30 +0800

    dev: 返回不同响应

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
   def create
-    u = User.new
-    u.email = params[:email]
-    u.password = params[:password]
-    u.save
+    user = User.new
+    user.email = params[:email]
+    user.password = params[:password]
+    user.password_confirmation = params[:password_confirmation]
+    if user.save
+      render json: {resource: user},status: 200
+    else
+      render json: {errors: user.errors},status: 400
+    end
+
   end
 end
