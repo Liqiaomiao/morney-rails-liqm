c5f71c1cfe4488c840f9e14f48c9c71677045c16
Liqiaomiao <1049259456@qq.com>
2020-07-01 09:20:08 +0800

    dev: 代码优化2-参数生成提取

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
   def create
-    a = params.permit(:email,:password, :password_confirmation)
-    user = User.new(a)
+    user = User.new create_params
     if user.save
       render json: {resource: user},status: 200
     else
       render json: {errors: user.errors},status: 400
     end
 
+  end
+  def create_params
+    params.permit(:email,:password, :password_confirmation)
   end
 end
