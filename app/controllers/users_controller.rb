d39a0f58ec3f0f4bd96071738a9351f97c4cb4ef
Liqiaomiao <1049259456@qq.com>
2020-07-02 08:34:18 +0800

    dev: 代码优化4-服务端输出内容方法提取成公共方法

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,4 @@ class UsersController < ApplicationController
   def create_params
     params.permit(:email,:password, :password_confirmation)
   end
-  def render_resource resource
-    if resource.valid?
-      render json: {resource: resource},status: 200
-    else
-      render json: {errors: resource.errors},status: 400
-    end
-  end
 end
