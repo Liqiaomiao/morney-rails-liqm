386eb6c3a8e4dc7fa9f14adb7cab562b354dc1fe
Liqiaomiao <1049259456@qq.com>
2020-07-13 09:12:23 +0800

    feat: 登录验证密码校验

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,18 +1,24 @@
 class Session
   include ActiveModel::Model
-  attr_accessor :email, :password
+  attr_accessor :email, :password, :user
   validates :email, presence: true
   validate :check_email, if: :email
   validates :password, presence: true
 
   validates_format_of :email, with: /.+@.+/, if: :email
   validates :password, length: {minimum:6}, if: :password
-
+  validate :check_password , if: Proc.new { |s| s.email.present? and s.password.present? }
   def check_email
-    user = User.find_by_email email
+    user ||= User.find_by_email email
     p user
     if user.nil?
       errors.add :email, :not_found
+    end
+  end
+  def check_password
+    user ||= User.find_by_email email
+    if user and not user.authenticate(password)
+        errors.add :password, :not_match
     end
   end
 end
