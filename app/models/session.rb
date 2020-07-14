7c503cc7cb23af35c49c868ffcc6f5e22cd662f2
Liqiaomiao <1049259456@qq.com>
2020-07-14 09:05:29 +0800

    feat: session 存储

--- app/models/session.rb
+++ app/models/session.rb
@@ -9,14 +9,14 @@ class Session
   validates :password, length: {minimum:6}, if: :password
   validate :check_password , if: Proc.new { |s| s.email.present? and s.password.present? }
   def check_email
-    user ||= User.find_by_email email
+    @user ||= User.find_by_email email
     p user
     if user.nil?
       errors.add :email, :not_found
     end
   end
   def check_password
-    user ||= User.find_by_email email
+    @user ||= User.find_by_email email
     if user and not user.authenticate(password)
         errors.add :password, :not_match
     end
